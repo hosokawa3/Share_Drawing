8b2b11c579970b95b869cdb7feb6c9ad25f7c26d
EC2 Default User <ec2-user@ip-172-31-34-130.ap-northeast-1.compute.internal>
2024-03-03 11:44:04 +0000

    [Add]top_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
 
+  # namespace :public do
+    # get 'homes/top'
+    # get 'homes/about'
+  # end
+  # namespace :public do
+    # get 'end_users/show'
+    # get 'end_users/edit'
+  # end
+
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about' => "homes#about"
+    resources :end_users, only: [:show, :edit]
+  end
+
   #管理者側
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: 'admin/sessions'
