cb59686903f20be5ea2a24946f66420356c1048d
EC2 Default User <ec2-user@ip-172-31-34-130.ap-northeast-1.compute.internal>
2024-03-13 08:01:16 +0000

    [Update]dm

--- app/controllers/public/messages_controller.rb
+++ app/controllers/public/messages_controller.rb
@@ -1,2 +1,19 @@
 class Public::MessagesController < ApplicationController
+  before_action :authenticate_end_user!
+
+  def create
+    message = Message.new(message_params)
+    message.end_user_id = current_end_user.id
+    if message.save
+      redirect_to room_path(message.room)
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:room_id, :message)
+  end
 end
