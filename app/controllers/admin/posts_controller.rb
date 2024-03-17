ca35f4ff1cbbada192739ad3d05fcc204bb8af95
EC2 Default User <ec2-user@ip-172-31-34-130.ap-northeast-1.compute.internal>
2024-03-17 13:16:33 +0000

    [Add]img

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,11 +2,23 @@ class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-     @q = Post.ransack(params[:q])
-    if params[:q].present?
-      @posts = @q.result(distinct: true)
+    @q = Post.ransack(title_or_end_user_name_cont: params[:title_or_end_user_name_cont])
+    if params[:title_or_end_user_name_cont].present?
+      @posts = @q.result(distinct: true).page(params[:page]).per(8)
     else
-      @posts = Post.all
+      @posts = Post.all.order(created_at: :desc).page(params[:page]).per(8)
+    end
+
+    if params[:latest]
+      @posts = Post.latest.page(params[:page]).per(8)
+    elsif params[:old]
+      @posts = Post.old.page(params[:page]).per(8)
+    elsif params[:most_favorited]
+      @posts = Kaminari.paginate_array(Post.most_favorited).page(params[:page]).per(8)
+    else
+      unless params[:title_or_end_user_name_cont].present?
+        @Posts = Post.all.order(created_at: :desc).page(params[:page]).per(8)
+      end
     end
   end
 
